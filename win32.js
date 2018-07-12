76aa35b6c4086285b107d28ec681caecd954549f
Jussi Vatjus-Anttila <jussi.vatjus-anttila@arm.com>
2018-07-12 11:52:27 +0300

    allows another date format for win32 and avoid crash

--- win32.js
+++ win32.js
@@ -1,12 +1,33 @@
 'use strict';
-const execSync = require('child_process').execSync;
+const {uptime} = require('os');
+const {execSync} = require('child_process');
 
 module.exports = () => {
   const output = execSync('net statistics workstation').toString();
-  const date = output.match(/((\d{2}\.){2}\d{4}){1} ((\d{2}:){2}\d{2}){1}/gm);
-  const split = date[0].split(' ');
-  const format = split[0].split('.');
-  const formatedDate = `${format[1]}-${format[0]}-${format[2]} ${split[1]}`;
-
+  const formats = [
+    {
+      regex: /((\d{2}\.){2}\d{4}){1} ((\d{2}:){2}\d{2}){1}/gm,
+      parser: (match) => {
+        const split = match[0].split(' ');
+        const format = split[0].split('.');
+        return `${format[1]}-${format[0]}-${format[2]} ${split[1]}`;
+      }
+    },
+    {
+      regex: /((\d{1,2}\.){2}\d{4}){1} ((\d{1,2}\.){2}(\d{1,2}))/gm,
+      parser: (match) => {
+        const split = match[0].split(' ');
+        const format = split[0].split('.');
+        const time = split[1].split('.').join(':');
+        return `${format[1]}-${format[0]}-${format[2]} ${time}`;
+      }
+    },
+    {
+      regex: /.*/,
+      parser: () => new Date(new Date() - uptime())
+    }
+  ];
+  const format = formats.find((candidate) => output.match(candidate.regex));
+  const formatedDate = format.parser(output.match(format.regex));
   return new Date(formatedDate);
 };
